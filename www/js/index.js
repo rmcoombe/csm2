5002695cdcbdd19b7aea4e7f44f88258522f615e
rmcoombe <rosscoombe@gmail.com>
2020-09-13 18:04:30 +0100

    gfth

--- www/js/index.js
+++ www/js/index.js
@@ -2,7 +2,7 @@ function crewScan(){
 cordova.plugins.barcodeScanner.scan(
       function (result) {
             document.getElementById("varname").value = result.text;
-            eventFire(document.getElementById('varname'), 'click');
+            //eventFire(document.getElementById('varname'), 'click');
           
       },
       function (error) {
@@ -24,27 +24,3 @@ cordova.plugins.barcodeScanner.scan(
    );
 };
 
-function carScan(){
-cordova.plugins.barcodeScanner.scan(
-      function (result) {
-            document.getElementById("car").value = result.text;
-          
-      },
-      function (error) {
-          alert("Scanning failed: " + error);
-      },
-      {
-          preferFrontCamera : false, // iOS and Android
-          showFlipCameraButton : false, // iOS and Android
-          showTorchButton : false, // iOS and Android
-          torchOn: false, // Android, launch with the torch switched on (if available)
-          saveHistory: true, // Android, save scan history (default false)
-          prompt : "Place a barcode inside the scan area", // Android
-          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-          formats : "CODE_128", // default: all but PDF_417 and RSS_EXPANDED
-          orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
-          disableAnimations : true, // iOS
-          disableSuccessBeep: false // iOS and Android
-      }
-   );
-};
