8506b65b2973a16ae0cd84c1d20afa0d0ba4ec30
rmcoombe <rosscoombe@gmail.com>
2020-09-13 17:37:58 +0100

    fgbb
    fgxb

--- www/json.php
+++ www/json.php
@@ -1,7 +1,12 @@
 <?php
-include "db.php"; 
-$data=array(); 
-$q=mysqli_query($con, "select * from `tbl_employee` WHERE EmployeeID = '403'"); 
+include "db.php";
+if (isset($_GET['cm'])) {
+    $cm = $_GET['cm'];
+} else {
+    $cm = "9999999";
+}
+$data=array();
+$q=mysqli_query($con, "select * from `tbl_employee` WHERE EmployeeID = $cm"); 
 while ($row=mysqli_fetch_object($q)){
     $data[]=$row; 
 }
